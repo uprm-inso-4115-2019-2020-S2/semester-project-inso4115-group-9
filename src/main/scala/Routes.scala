519430f1d5eede68be418dae26a38d812430fa8b
Alejandro Matos Mercado <alejandro,matos1@upr.edu>
2020-03-30 14:06:51 -0400

    Cleaning branch for merge

--- src/main/scala/Routes.scala
+++
@@ -1,9 +0,0 @@
-import akka.http.scaladsl.server.Directives._
-import api.{ApiErrorHandler, UsersApi}
-
-trait Routes extends ApiErrorHandler with UsersApi{
-  val routes =
-    pathPrefix("rest") {
-      usersApi
-    } ~ path("")(getFromResource("public/index.html"))
-}
--- src/main/scala/models/User.scala
+++
@@ -1,3 +0,0 @@
-package models
-
-case class User(id: Option[UserId], firstname: String, lastname: String, email: String, gender: Char, date_birth: String, request_completed: Int)
